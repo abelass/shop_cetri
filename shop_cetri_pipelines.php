3999e3d78fe78cafaa95ef00a439a198de8809b8
Rainer <rainer@websimple.be>
2014-12-30 15:24:04 +0100

    pas de js pour le moment

--- shop_cetri_pipelines.php
+++ shop_cetri_pipelines.php
@@ -15,7 +15,7 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 function shop_cetri_insert_head($flux) {
 	$css = find_in_path('css/shop_cetri.css');
 	$js = find_in_path('javascript/shop_cetri.js');
-	$flux .= "<script  type='text/javascript' src='$js' /></script>\n";
+	//$flux .= "<script  type='text/javascript' src='$js' /></script>\n";
 	$flux .= "<link rel='stylesheet' type='text/css' media='all' href='$css' />\n";
 	
 	return $flux;
