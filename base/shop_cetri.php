f0c85e48ca35befbd3e7d85aa65b1da743e8976a
Rainer Müller <rainer@websimple.be>
2017-12-16 11:17:48 +0100

    formatage

--- base/shop_cetri.php
+++ base/shop_cetri.php
@@ -6,72 +6,69 @@
  * @author     Rainer Müller
  * @licence    GNU/GPL
  */
-
-if (!defined('_ECRIRE_INC_VERSION')) return;
-
-
-
+if (! defined('_ECRIRE_INC_VERSION'))
+	return;
 
 // Ajout de champs extras
-function shop_cetri_declarer_champs_extras($champs = array()) {
-
+function shop_cetri_declarer_champs_extras ($champs = array()) {
 	$champs['spip_articles']['lang'] = array(
 
-			'saisie' => 'input',//Type du champ (voir plugin Saisies)
+		'saisie' => 'input', // Type du champ (voir plugin Saisies)
 
-			'options' => array(
-					'nom' => 'prix',
-					'label' => _T('prix'),
-					'sql' => "float (38,2) NOT NULL",
-					'defaut' => '',// Valeur par défaut
-				),
+		'options' => array(
+			'nom' => 'prix',
+			'label' => _T('prix'),
+			'sql' => "float (38,2) NOT NULL",
+			'defaut' => '' // Valeur par défaut
+		)
 
 	);
 
 	return $champs;
-
 }
 
-/*champ extra shop -> pour la commande*/
-function inc_shop_cetri_shop_champs_extras($flux) {
-	$defaut=$flux['args'];
-	
-	$flux['data']['commande']['saisies'][]=
-		array(
-                    'saisie' => 'oui_non',// Pour la configuration               
-                    'formulaires' => array('configuer_shop','editer_client'),
-                    'tables'=>array(
-                        'spip_commandes'=>array(
-                            'field'=>array(
-                                'newsletter'=>"varchar(3) NOT NULL"
-                                )
-                             )
-                        ),
-                    'options' => array(
-                        'li_class'=>'conditionnel',
+/* champ extra shop -> pour la commande */
+function inc_shop_cetri_shop_champs_extras ($flux) {
+	$defaut = $flux['args'];
 
-                        'nom' => 'commande_newsletter',// Pour la configuration
-                        'nom_2' => 'newsletter',// Pour le formulaire public                          
-                        'label' => _T('shop_cetri:label_inscrire_newsletter'),
-                        'defaut' => isset($defaut['commande_newsletter'])?$defaut['commande_newsletter']:'',
+	$flux['data']['commande']['saisies'][] = array(
+		'saisie' => 'oui_non', // Pour la configuration
+		'formulaires' => array(
+			'configuer_shop',
+			'editer_client'
+		),
+		'tables' => array(
+			'spip_commandes' => array(
+				'field' => array(
+					'newsletter' => "varchar(3) NOT NULL"
+				)
+			)
+		),
+		'options' => array(
+			'li_class' => 'conditionnel',
 
-                    )
+			'nom' => 'commande_newsletter', // Pour la configuration
+			'nom_2' => 'newsletter', // Pour le formulaire public
+			'label' => _T('shop_cetri:label_inscrire_newsletter'),
+			'defaut' => isset($defaut['commande_newsletter']) ? $defaut['commande_newsletter'] : ''
 
-	 
-                );
-	$flux['data']['commande']['saisies'][]=
-		                array(
-                    'saisie' => 'oui_non',
-                    'formulaires' => array('configuer_shop'),
-                    'options' => array(
-                        'li_class'=>'conditionnel last',
-                        'nom' => 'newsletter_obligatoire',
-                        'label' => _T('saisies:option_obligatoire_label'),
-                        'defaut' => isset($defaut['newsletter_obligatoire'])?$defaut['newsletter_obligatoire']:'',
-                        'afficher_si' => '@commande_newsletter@ == "on"',
-                    )
-                );
-				
+		)
+
+	);
+	$flux['data']['commande']['saisies'][] = array(
+		'saisie' => 'oui_non',
+		'formulaires' => array(
+			'configuer_shop'
+		),
+		'options' => array(
+			'li_class' => 'conditionnel last',
+			'nom' => 'newsletter_obligatoire',
+			'label' => _T('saisies:option_obligatoire_label'),
+			'defaut' => isset($defaut['newsletter_obligatoire']) ? $defaut['newsletter_obligatoire'] : '',
+			'afficher_si' => '@commande_newsletter@ == "on"'
+		)
+	);
+
 	return $flux;
 }
 ?>
